487fc442f873a5df03af0a2ed1bb85c8790aa5b5
kelbek <dev_kelbek@mail.ru>
2018-08-18 01:41:10 +0700

    added snap

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import locale
+from system import system
+from install import installing as inst
+
+locale.setlocale(locale.LC_ALL, '')
+
+systemObj = system.System()
+systemObj.verified()
+
+instObj = inst.Installing()
+instObj.go()
